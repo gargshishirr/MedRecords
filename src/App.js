01d3fa0ceea38627c1936de8e5c4190622ce7bc3
gargshishirr <shishirgarg_co21a10_35@dtu.ac.in>
2023-07-02 02:26:35 +0530

    intial comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css' 
+import SideBar from './components/SideBar Section/SideBar'
+const App=()=>{
+    return (
+       <div className='container'>
+        <Sidebar/>
+        <Body/>
+ 
+       </div> 
+    )
+}
+
+export default App
---
+++ src/components/SideBar Section/SideBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SideBar.css'
+
+import img from 
+const SideBar = () => {
+  return (
+    <div className="sideBar grid">
+        <div className="logoDiv flex">
+            <img src="" alt="Image Name" />
+            <h2> Medical Records</h2>
+
+        </div>
+    
+    </div>
+  )
+}
+
+export default SideBar
